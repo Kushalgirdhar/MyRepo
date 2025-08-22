ffde74d79ee23a377dc953e4693108962d9057ed
Kushal <kushalgirdhar31@gmail.com>
2025-08-22 19:06:29 +0530

    Add more new files

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,11 @@
-import { useActionState } from "react";
-export default function App(){
-  const handleSubmit=(previousData,formData)=>{
-    let name=formData.get('name');
-    let password=formData.get('password')
-    console.log("handleSubmit called",name,password);    
-  }
-  const[data,action,pending]=useActionState(handleSubmit,undefined)
-  return(
+
+
+export default function APP() {
+
+  return (
     <>
-    <div>
-      <h1>UseActionState Hook in React Js</h1>
-      <form action={action}>
-        <input type="text" placeholder="enter name" name="name"/>
-        <br /><br />
-        <input type="password" placeholder="enter Password" name="password" />
-        <br /><br />
-        <button>Submit Data</button>
-      </form>
-    </div>
+    <h1 className="bg-red-700 text-3xl border-y-purple-600">This is everything</h1>
 
     </>
-
-
-  )
+  );
 }
---
+++ src/ClassComponent.jsx
@@ -0,0 +1,9 @@
+import Student2 from "./Student2"
+export default function ClassComponent(){
+    return(
+        <div style={{backgroundColor:"skyblue",padding:10}}>
+            <h1>Class Component</h1>
+            <Student2></Student2>
+        </div>
+    )
+}
